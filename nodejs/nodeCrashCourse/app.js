1286961927df0672ad2dd839f10a4bfe4fb2bf14
Pedro Frohmut <pedrofrohmut@gmail.com>
2018-04-28 22:19:03 -0300

    Formating

--- nodejs/nodeCrashCourse/app.js
+++ nodejs/nodeCrashCourse/app.js
@@ -2,22 +2,22 @@
 const http = require('http')
 const fs = require('fs')
 
-const hostName  = '127.0.0.1'
-const port      = 3000
+const hostName = '127.0.0.1'
+const port = 3000
 
 fs.readFile('index.html', (err, html) => {
-  if (err) {
-    throw err
-  } else {
-    const server = http.createServer((req, res) => {
-      res.statusCode = 200
-      res.setHeader('Content-type', 'text/html')
-      res.write(html)
-      res.end()
-    })
+    if (err) {
+        throw err
+    } else {
+        const server = http.createServer((req, res) => {
+            res.statusCode = 200
+            res.setHeader('Content-type', 'text/html')
+            res.write(html)
+            res.end()
+        })
 
-    server.listen(port, hostName, () => {
-      console.log('Server started on port ' + port)
-    })
-  }
+        server.listen(port, hostName, () => {
+            console.log('Server started on port ' + port)
+        })
+    }
 })
